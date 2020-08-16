5742d645f92821573f07c49a2bafd33b3fa030cf
Julien BOYER <julboyer@e3r4p17.42.fr>
2020-08-16 12:58:49 +0200

    tests

---
+++ 2_0/includes/libftprintf.h
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julboyer <julboyer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 17:38:25 by julboyer          #+#    #+#             */
+/*   Updated: 2020/08/16 12:57:37 by julboyer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+
+# define LIBFTPRINTF_H
+# include <string.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+# include <wchar.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+typedef union			u_type
+{
+	void				*p;
+	long long int		lli;
+	unsigned long long	ulli;
+	char				*s;
+	wchar_t				*ls;
+	double				lf;
+	long double			llf;
+	short int			*sn;
+	int					*n;
+	long int			*ln;
+	long long int		*lln;
+}						t_type;
+
+typedef struct			s_flags
+{
+	char				convert;
+	int					type;
+	char				pref;
+	int					prec;
+	char				flag_width;
+	int					width;
+	t_type				u;
+}						t_flags;
+
+int						get_next_line(int fd, char **line);
+int						ft_isalpha(int c);
+size_t					ft_strlen(const char *str);
+int						ft_isdigit(int c);
+int						ft_isalnum(int c);
+int						ft_toupper(int c);
+int						ft_tolower(int c);
+int						ft_atoi(const char *str);
+char					*ft_strchr(const char *s, int c);
+char					*ft_strrchr(const char *s, int c);
+char					*ft_strnstr(const char *big,
+						const char *little, size_t len);
+size_t					ft_strlcpy(char *dst, const char *src, size_t size);
+size_t					ft_strlcat(char *dst, const char *src, size_t size);
+void					*ft_bzero(void *s, size_t n);
+void					*ft_memccpy(void *dest, const void *src, int c,
+						size_t n);
+void					*ft_memset(void *s, int c, size_t n);
+char					*ft_strncpy(char *dest, char *src, unsigned int n);
+char					*ft_strdup(char *str);
+int						ft_memcmp(const void *s1, const void *s2, size_t n);
+void					*ft_memmove(void *dest, const void *src, size_t n);
+void					*ft_memcpy(void *dest, const void *src, size_t n);
+void					*ft_calloc(size_t nmemb, size_t size);
+char					*ft_substr(char const *s, unsigned int start,
+						size_t len);
+char					*ft_strjoin(char const *s1, char const *s2);
+char					**ft_split(char const *s, char c);
+void					ft_putstr_fd(char *s, int fd);
+void					ft_putchar_fd(char c, int fd);
+char					*ft_itoa(int n);
+void					ft_putendl_fd(char *s, int fd);
+void					ft_putnbr_fd(int n, int fd);
+char					*ft_strmapi(char const *s, char (*f)(unsigned int,
+						char));
+char					*ft_strtrim(char const *s1, char const *set);
+int						ft_strncmp(const char *s1, const char *s2, size_t n);
+void					*ft_memchr(const void *s, int c, size_t n);
+int						ft_isascii(int c);
+int						ft_isprint(int c);
+t_list					*ft_lstnew(void *content);
+void					ft_lstadd_front(t_list **alst, t_list *new_list);
+t_list					*ft_lstlast(t_list *lst);
+void					ft_lstadd_back(t_list **alst, t_list *new_list);
+void					ft_lstdelone(t_list *llst, void (*del)(void *));
+void					ft_lstclear(t_list **lst, void (*del)(void *));
+int						ft_lstsize(t_list *lst);
+void					ft_lstiter(t_list *lst, void (*f)(void *));
+t_list					*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
+char					*ft_strcat(char *dest, char *src);
+char					*ft_strcpy(char *dest, char *src);
+char					*ft_strncpy(char *dest, char *src, unsigned int n);
+char					*ft_ltoa(long int n);
+
+char					*ft_ulltoa_base(unsigned long long int n,
+		char *base);
+int						ft_printf(const char *s,
+		...) __attribute__((format(printf,1,2)));
+char					*ft_width(char *s, t_flags flags);
+
+
+#endif
--- 2_0/srcs/ft_shandle.c
+++ 2_0/srcs/ft_shandle.c
@@ -5,49 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: julboyer <julboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/10 13:34:35 by julboyer          #+#    #+#             */
-/*   Updated: 2020/08/16 11:54:52 by julboyer         ###   ########.fr       */
+/*   Created: 2020/08/16 11:04:33 by julboyer          #+#    #+#             */
+/*   Updated: 2020/08/16 12:27:17 by julboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "ft_printf.h"
-
-int		ft_lshandle(t_flags flags, va_list params)
-{
-	wchar_t	*res_s;
-	wchar_t *tmp;
-	int		i;
-
-	i = 0;
-	tmp = va_arg(params, wchar_t*);
-	res_s = ft_lstrdup(tmp ? tmp : L"(null)");
-	if (flags.prec >= 0)
-		res_s = ft_ldot_flag(res_s, flags.prec);
-	res_s = ft_lwidth(res_s, flags.width, flags.flag_width);
-	i = ft_putwstr(res_s);
-	free(res_s);
-	return (i);
-}
+#include "libft.h"
 
 int		ft_shandle(t_flags flags, va_list params)
 {
 	char	*res_s;
-	int		i;
 	char	*tmp;
+	int		i;
+	int		len;
 
-	i = 0;
-	res_s = NULL;
-	tmp = NULL;
 	if (flags.type == 1)
 		return (ft_lshandle(flags, params));
 	tmp = va_arg(params, char*);
-	res_s = ft_strdup(tmp ? tmp : "(null)");
-	if (flags.prec >= 0)
-		res_s = ft_dot_flag(res_s, flags.prec, flags);
-	res_s = ft_width(res_s, flags.width, flags);
-	while (res_s[i])
-		ft_putchar_fd(res_s[i++], 1);
-	free(res_s);
-	return (i);
+	res_s = tmp ? tmp : "(null)";
+	i = ft_strlen(res_s);
+	i = (flags.prec > -1 && flags.prec < i) ? flags.prec : i;
+	len = (i > flags.width) ? i : flags.width;
+	tmp = ft_calloc(sizeof(*tmp) * len);
+	tmp = ft_strncpy(tmp, res_s, i);
+	
 }
